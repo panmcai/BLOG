5cdcea40774c4cc8644625617811328e68c4621e
panmcai <panmcai@foxmail.com>
2024-12-09 23:44:00 +0800

    修改title

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -6,8 +6,8 @@ export default defineUserConfig({
   base: "/blog/",
 
   lang: "zh-CN",
-  title: "博客演示",
-  description: "vuepress-theme-hope 的博客演示",
+  title: "Peter·Pan的博客",
+  description: "这是Peter·Pan的个人博客网站",
 
   theme,
 
